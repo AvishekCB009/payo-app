50fccfc7a7b8cee35a0f943b080e178377112e09
AvishekCB009 <toavishek.official@gmail.com>
2025-08-23 19:53:34 +0600

    log in

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// log in fuctionalities
+document.getElementById('log_in_btn')
+    .addEventListener('click',function(e){
+        e.preventDefault()
+        const mNumber = 00123456789;
+        const pinNumber = 1234;
+        const mNumberVal = document.getElementById('mobile_number').value
+        const convertedMNumber = parseInt(mNumberVal)
+        const pinNumberVal = document.getElementById('pin_number').value
+        const convertedPinNumber = parseInt(pinNumberVal)
+        if(convertedMNumber === mNumber && convertedPinNumber === pinNumber){
+          window.location.href="./page/home.html"
+        }})
