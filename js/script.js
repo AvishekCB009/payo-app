93de001c29a96b1d307b46b594096b9cf4decffc
AvishekCB009 <toavishek.official@gmail.com>
2025-08-23 21:13:10 +0600

    home update

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
 document.getElementById('log_in_btn')
     .addEventListener('click',function(e){
         e.preventDefault()
-        const mNumber = 00123456789;
+        const mNumber = 12345678900;
         const pinNumber = 1234;
         const mNumberVal = document.getElementById('mobile_number').value
         const convertedMNumber = parseInt(mNumberVal)
         const pinNumberVal = document.getElementById('pin_number').value
         const convertedPinNumber = parseInt(pinNumberVal)
         if(convertedMNumber === mNumber && convertedPinNumber === pinNumber){
-          window.location.href="./page/home.html"
+          window.location.href="./pages/home.html"
         }})
