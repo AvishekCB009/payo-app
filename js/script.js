e5633e50f065a836d476d488d0253fcd10f0ef2b
AvishekCB009 <toavishek.official@gmail.com>
2025-08-24 12:39:42 +0600

    update add withdraw

--- js/script.js
+++ js/script.js
@@ -1,13 +1,70 @@
-// log in fuctionalities
-document.getElementById('log_in_btn')
-    .addEventListener('click',function(e){
-        e.preventDefault()
-        const mNumber = 12345678900;
-        const pinNumber = 1234;
-        const mNumberVal = document.getElementById('mobile_number').value
-        const convertedMNumber = parseInt(mNumberVal)
-        const pinNumberVal = document.getElementById('pin_number').value
-        const convertedPinNumber = parseInt(pinNumberVal)
-        if(convertedMNumber === mNumber && convertedPinNumber === pinNumber){
-          window.location.href="./pages/home.html"
-        }})
+// toggle
+document.getElementById('grid_menu_add')
+    .addEventListener('click',function(){
+        document.getElementById('cashout_money_form').style.display= 'none'
+        document.getElementById('add_money_form').style.display = 'block'
+        document.getElementById('grid_menu_add').classList.add('selected_indicator')
+        document.getElementById('grid_menu_cashout').classList.remove('selected_indicator')
+
+    })
+document.getElementById('grid_menu_cashout')
+    .addEventListener('click',function(){
+        document.getElementById('add_money_form').style.display= 'none'
+        document.getElementById('cashout_money_form').style.display = 'block'
+        document.getElementById('grid_menu_cashout').classList.add('selected_indicator')
+        document.getElementById('grid_menu_add').classList.remove('selected_indicator')
+    })
+
+const validPin = 4321
+document.getElementById('add_money_btn')
+    .addEventListener(
+        'click',function(e){
+            e.preventDefault()
+            const bank = document.getElementById('bank_name').value
+            const addAccountNum = document.getElementById('account_number_to_add').value
+            const addAmount = parseInt(document.getElementById('added_amount').value)
+            const pin = parseInt(document.getElementById('send_pin_number').value)
+            const balance = parseInt(document.getElementById('balance').innerText)
+
+            if(addAccountNum.length!==11){
+                alert('Please provide valid 11 digit account number')
+                return;
+            }
+            if(pin!==validPin){
+                alert('please provide valid pin')
+                return;
+            }
+            // if(typeof addAmount !== 'number'){
+            //     alert('enter valid amount to add')
+            //     return;
+            // }
+            const newBalance = addAmount+balance
+            document.getElementById('balance').innerText = newBalance
+        }
+    )
+
+document.getElementById('withdraw_money_btn')
+    .addEventListener(
+        'click',function(e){
+            e.preventDefault()
+            const withdrawAccountNum = document.getElementById('agent_number_to_withdraw').value
+            const withdrawAmount = parseInt(document.getElementById('withdraw_amount').value)
+            const pin = parseInt(document.getElementById('withdraw_pin_number').value)
+            const balance = parseInt(document.getElementById('balance').innerText)
+
+            if(withdrawAccountNum.length!==11){
+                alert('Please provide valid 11 digit account number')
+                return;
+            }
+            if(pin!==validPin){
+                alert('please provide valid pin')
+                return;
+            }
+            // if(typeof addAmount !== 'number'){
+            //     alert('enter valid amount to add')
+            //     return;
+            // }
+            const newBalance = balance-withdrawAmount
+            document.getElementById('balance').innerText = newBalance
+        }
+    )
